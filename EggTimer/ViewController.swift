a6f6121572250d2a65e711fb741519cff6ed8fe2
Raiyan <raiyan.sharif.1234@gmail.com>
2020-07-09 00:07:30 +0600

    Timer and functionality set and code polished

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,6 +10,52 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    let eggTimes = ["Soft": 300, "Medium": 4, "Hard":7]
 
+    @IBOutlet weak var titlleLabel: UILabel!
+    
+    var secondsRemaining = 60
+    
+    @IBOutlet weak var progressBar: UIProgressView!
+    var timer = Timer()
+    var valHardness:Float = 0.0
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+//        print(sender.currentTitle!)
+        progressBar.progress = 0.0
+        timer.invalidate()
+        let hardness = sender.currentTitle!
+        titlleLabel.text = "\(hardness) is Boiling"
+        secondsRemaining = eggTimes[hardness]!
+        valHardness = Float(secondsRemaining)
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateTimer(){
+        if secondsRemaining > 0{
+//            print("\(secondsRemaining) seconds")
+            if valHardness - Float(secondsRemaining) == 0{
+                progressBar.progress = 0.0
+            }
+            else{
+            progressBar.progress = (valHardness - Float(secondsRemaining)) / valHardness
+            }
+            
+            secondsRemaining -= 1
+        }
+        
+        else{
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+                self.progressBar.progress = 0.0
+                self.titlleLabel.text = "How do you like your eggs?"
+            }
+            timer.invalidate()
+            progressBar.progress = 1.0
+            titlleLabel.text = "Done"
+            
+            
+            
+        }
+    }
+    
 
 }
